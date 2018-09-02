1b3c6d0fb4f016ded65eb9e7d1d79699f8dd91f6
MrDanii <dan333_d@hotmail.com>
2018-09-02 15:14:06 -0500

    ejercicio #4 Serial

--- Ejercicios de clase/sketch4serial_aug30b/sketch4serial_aug30b.ino
+++ Ejercicios de clase/sketch4serial_aug30b/sketch4serial_aug30b.ino
@@ -1,9 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+char inputData [17];
+
+void setup() {  
+  Serial.begin(9600);               // Es la velocidad del serial dado en bits / segundo
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(Serial.available() > 0){       // Solo si el serial está disponible
+    delay(100);
+    int i = 0;
+    while(Serial.available() > 0){
+      inputData[i] = Serial.read();
+      i ++;
+    }
+    Serial.println(inputData);
+    for(int j = 0; j < 17; j++)     // Limpiamos nuestro arreglo de carácteres
+      inputData[i] = 0;
+  }
 }
